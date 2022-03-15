86d55504a84c735c55576490f43d2018b8e76a8c
Jongyeol <45934107+Jongye0l@users.noreply.github.com>
2022-03-15 23:13:53 +0900

    1.2 pre1
    광고 추가

--- app/src/main/java/com/Jongyeol/hsmsmenu/MainActivity.java
+++ app/src/main/java/com/Jongyeol/hsmsmenu/MainActivity.java
@@ -8,6 +8,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -18,16 +24,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
     String nums;
     TextView textView1;
     TextView textView2;
     Date date = new Date();
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
         textView1 = (TextView) findViewById(R.id.lunch);
         textView2 = (TextView) findViewById(R.id.dinner);
         Reload();
@@ -89,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
     }
-
     Handler handler1 = new Handler(){
         @Override
         public void handleMessage(Message msg) {
